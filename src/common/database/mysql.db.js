54b0cae6f19914625d37d1c494b1c9c9b3b33376
sknacook03 <pongwit27@gmail.com>
2024-09-26 09:55:00 +0700

    update

--- src/common/database/mysql.db.js
+++ src/common/database/mysql.db.js
@@ -1,34 +1,34 @@
-import mysql from 'mysql'
-import dotenv from 'dotenv'
+// import mysql from 'mysql'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
-//กำหนดการเชื่อมต่อฐานข้อมูล:
-const db = mysql.createConnection({
-    host    : process.env.MYSQL_HOST, // หรือชื่อโฮสต์ของ MySQL server
-    user    : process.env.MYSQL_USER, // ชื่อผู้ใช้ MySQL
-    password: process.env.MYSQL_PASSWORD, // รหัสผ่าน MySQL
-    database: process.env.MYSQL_DB_NAME, // ชื่อฐานข้อมูล MySQL ที่ต้องการเชื่อมต่อ
-  });
+// //กำหนดการเชื่อมต่อฐานข้อมูล:
+// const db = mysql.createConnection({
+//     host    : process.env.MYSQL_HOST, // หรือชื่อโฮสต์ของ MySQL server
+//     user    : process.env.MYSQL_USER, // ชื่อผู้ใช้ MySQL
+//     password: process.env.MYSQL_PASSWORD, // รหัสผ่าน MySQL
+//     database: process.env.MYSQL_DB_NAME, // ชื่อฐานข้อมูล MySQL ที่ต้องการเชื่อมต่อ
+//   });
 
-//เปิดการเชื่อมต่อ MySQL ด้วยเมธอด connect()
-db.connect((error) => {
-    if (error) {
-      console.error(`Not connect MySQL: ${error}`);
-      return;
-    }
-    //แสดงหมายเลขที่ใช้ระบุแต่ละการเชื่อมต่อที่ MySQL Server 
-    console.log(`connected MySQL as id: ${db.threadId}`);
-});  
-export const doQuery = (sql, options)=>{
-    return new Promise ((resolve,reject)=>{
-        db.query(sql, options, (err, res)=>{
-            if(err){
-                throw err
-            }
-            resolve(res)
-        })
-    })
-}
+// //เปิดการเชื่อมต่อ MySQL ด้วยเมธอด connect()
+// db.connect((error) => {
+//     if (error) {
+//       console.error(`Not connect MySQL: ${error}`);
+//       return;
+//     }
+//     //แสดงหมายเลขที่ใช้ระบุแต่ละการเชื่อมต่อที่ MySQL Server 
+//     console.log(`connected MySQL as id: ${db.threadId}`);
+// });  
+// export const doQuery = (sql, options)=>{
+//     return new Promise ((resolve,reject)=>{
+//         db.query(sql, options, (err, res)=>{
+//             if(err){
+//                 throw err
+//             }
+//             resolve(res)
+//         })
+//     })
+// }
 
-export default db
+// export default db
--- src/index.route.js
+++ src/index.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import BookRouter from "./modules/books/book.route.js";
-import UserRouter from "./modules/user/user.route.js"
+// import UserRouter from "./modules/user/user.route.js"
 const IndexRouter = express();
 
 IndexRouter.use("/book", BookRouter);
-IndexRouter.use("/user", UserRouter);
+// IndexRouter.use("/user", UserRouter);
 export default IndexRouter;
